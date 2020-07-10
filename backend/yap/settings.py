9aabecacf721fe4c3fd612752e1f52daa0f0ad5f
Rafael Paixão <rafaelsspaixao@gmail.com>
2020-07-09 21:59:49 -0300

    add whitenoise for serving static files over gunicorn

--- backend/yap/settings.py
+++ backend/yap/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,6 +127,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'dist')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Heroku settings
 if ENVIROMENT == ENV_HEROKU:
