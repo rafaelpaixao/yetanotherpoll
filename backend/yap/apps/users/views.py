58aac26038eed5d24e0356749cd467ec53647799
Rafael Paixão <rafaelsspaixao@gmail.com>
2020-07-15 15:33:25 -0300

    remove unused

--- backend/yap/apps/users/views.py
+++ backend/yap/apps/users/views.py
@@ -1,11 +1,9 @@
-from django.contrib.auth.views import LogoutView
-from rest_framework import status, urls
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from .exceptions import UserInvalidCredentials, UserNotFound
 from .models import User
 from .serializers import AuthSerializer
 
