0db0860046c7838f66fcdc447ca9d3a38f12472b
Rafael Paixão <rafaelsspaixao@gmail.com>
2020-07-19 00:47:38 -0300

    reduce header size

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-row class="text-center my-5 py-5" align="center" justify="center">
+  <v-row class="text-center mt-5" align="center" justify="center">
     <v-col cols="12">
       <img :src="require('../assets/yap-logo.png')" />
     </v-col>
@@ -8,6 +8,6 @@
 
 <style lang="scss" scoped>
 img {
-  max-height: 6rem;
+  max-height: 5rem;
 }
 </style>
